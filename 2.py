d3c0951fc37c8873c027f649aa271a5e1266f03e
alpsr2006 <a.pisareva@g.nsu.ru>
2025-11-17 18:03:04 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,11 +1,28 @@
-import math
+import math, turtle as t
 xc = int(input(': '))
 yc = int(input(': '))
 r = int(input(': '))
 x = int(input(': '))
 y = int(input(': '))
-r_xy = math.sqrt(x ** 2 + y ** 2)
-if r_xy <= r:
-    print("Точка внутри окружности")
+#Вычисление расстояния от центра до точки
+d = math.sqrt((xc - x) ** 2 + (yc - y) ** 2)
+q = 0
+if d < r:
+    q = "Точка внутри окружности"
+elif d > r:
+    q = "Точка вне окружности"
 else:
-    print("Точка вне окружности")
+    q = "Точка на окружности"
+t.speed(5)
+t.penup()
+t.goto(xc, yc-r)
+t.pendown()
+t.circle(r)
+t.penup()
+t.goto(x, y)
+t.dot(5)
+t.hideturtle()
+t.goto(-60,-130)
+t.write(q)
+t.done()
+
